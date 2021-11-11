63c08ffb58d91d8ab85476e2aa1cd07452c2e3ac
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-11-10 21:45:13 -0800

    display product counts by category

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @product_count = Product.all.count
+    @apparel_count = Product.where(category_id: 1).count
+    @electronics_count = Product.where(category_id: 2).count
+    @furniture_count = Product.where(category_id: 3).count
   end
   
 end
